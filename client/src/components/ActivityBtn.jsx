73ab91f67d911ba9cda0c588017230196eada166
Guilherme Scotti <glscotti@hotmail.com>
2024-10-20 18:09:21 -0300

    fetch activities and increment votes

--- client/src/components/ActivityBtn.jsx
+++ client/src/components/ActivityBtn.jsx
@@ -1,20 +1,40 @@
-import React, {useState} from 'react';
-import './ActivityBtn.css'
+import React, { useState } from "react";
+import "./ActivityBtn.css";
 
-const ActivityBtn = (props) =>  {
+const ActivityBtn = (props) => {
+  const [num_votes, setNumVotes] = useState(props.num_votes);
 
-  const [num_votes, setNumVotes] = useState(props.num_votes)
+  const updateCount = async () => {
+    const options = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      //the server increments current votes by the number provided.
+      body: JSON.stringify({ num_votes: 1 }),
+    };
 
-  const updateCount = () => {
+    try {
+      const response = await fetch("/api/activities/" + props.id, options);
 
-  }
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      console.log("Post created successfully:", data);
+
+      setNumVotes((num_votes) => num_votes + 1);
+    } catch (error) {
+      console.error("Failed to create post:", error);
+    }
+  };
 
   return (
-    <button className='activityBtn' id={props.id} onClick={updateCount}>
-      {props.activity} <br/> {'△ ' + num_votes + ' Upvotes' }
+    <button className="activityBtn" id={props.id} onClick={updateCount}>
+      {props.activity} <br /> {"△ " + num_votes + " Upvotes"}
     </button>
-  )
-
-}
+  );
+};
 
 export default ActivityBtn;
