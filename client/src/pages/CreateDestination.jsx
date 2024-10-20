cbfb9f29f9e09a345a57e5a70045d18c1f63c1fc
Guilherme Scotti <glscotti@hotmail.com>
2024-10-20 17:27:01 -0300

    create destination

--- client/src/pages/CreateDestination.jsx
+++ client/src/pages/CreateDestination.jsx
@@ -1,79 +1,175 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import './CreateDestination.css'
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import "./CreateDestination.css";
 
 const CreateDestination = () => {
+  const [destination, setDestination] = useState({
+    destination: "",
+    description: "",
+    city: "",
+    country: "",
+    img_url: "",
+    flag_img_url: "",
+  });
+  const { trip_id } = useParams();
 
-    const [destination, setDestination] = useState({destination: "", description: "", city: "", country: "", img_url: "", flag_img_url: "" })
-    const {trip_id} = useParams();
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setDestination((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
 
-    const handleChange = (event) => {
-        const {name, value} = event.target;
-        setDestination( (prev) => {
-            return {
-                ...prev,
-                [name]:value,
-            }
-        })
-    }
-    
-    const createDestination = async (event) => {
-        
-        event.preventDefault();
+  const createDestination = async (event) => {
+    event.preventDefault();
 
+    const addDestination = async () => {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(destination),
+      };
 
-        const addDestination = async () => {
+      try {
+        const response = await fetch("/api/destinations", options);
 
- 
-        
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} - ${response.statusText}`);
         }
 
-        const createTripDestination = async (destination_id) => {
+        const data = await response.json();
+        console.log("Post created successfully:", data);
+        setDestination(data);
+        return data.id;
+      } catch (error) {
+        console.error("Failed to create post:", error);
+      }
+    };
 
+    const createTripDestination = async (destination_id) => {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          trip_id: trip_id,
+          destination_id: destination_id,
+        }),
+      };
 
-        
+      try {
+        const response = await fetch("/api/trips_destinations", options);
+
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} - ${response.statusText}`);
         }
 
+        const data = await response.json();
+        console.log("Post created successfully:", data);
 
-    }
+        return data;
+      } catch (error) {
+        console.error("Failed to create post:", error);
+      }
+    };
 
-    return (
-        <div>
-            <center><h3>Add Destination</h3></center>
-            <form>
-                <label>Destination</label> <br />
-                <input type="text" id="destination" name="destination" value={destination.destination} onChange={handleChange}/><br />
-                <br/>
+    addDestination()
+      .then((res) => createTripDestination(res))
+      .then((res) => (window.location = "/"));
+  };
 
-                <label>Description</label><br />
-                <textarea rows="5" cols="50" id="description" name="description" value={destination.description} onChange={handleChange}>
-                </textarea>
-                <br/>
+  return (
+    <div>
+      <center>
+        <h3>Add Destination</h3>
+      </center>
+      <form>
+        <label>Destination</label> <br />
+        <input
+          type="text"
+          id="destination"
+          name="destination"
+          value={destination.destination}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <label>Description</label>
+        <br />
+        <textarea
+          rows="5"
+          cols="50"
+          id="description"
+          name="description"
+          value={destination.description}
+          onChange={handleChange}
+        ></textarea>
+        <br />
+        <label>City </label>
+        <br />
+        <input
+          type="text"
+          id="city"
+          name="city"
+          value={destination.city}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <label>Country</label>
+        <br />
+        <input
+          type="text"
+          id="country"
+          name="country"
+          value={destination.country}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <label>Image URL </label>
+        <br />
+        <input
+          type="text"
+          id="img_url"
+          name="img_url"
+          value={destination.img_url}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <label>Flag Image URL</label>
+        <br />
+        <input
+          type="text"
+          id="flag_img_url"
+          name="flag_img_url"
+          value={destination.flag_img_url}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <label>Trip ID</label>
+        <br />
+        <input
+          type="text"
+          id="flag_img_url"
+          name="flag_img_url"
+          value={trip_id}
+          readOnly
+        />
+        <br />
+        <br />
+        <input type="submit" value="Submit" onClick={createDestination} />
+      </form>
+    </div>
+  );
+};
 
-                <label>City </label><br />
-                <input type="text" id="city" name="city" value={destination.city} onChange={handleChange}/><br />
-                <br/>
-
-                <label>Country</label><br />
-                <input type="text" id="country" name="country" value={destination.country} onChange={handleChange}/><br />
-                <br/>
-
-                <label>Image URL </label><br />
-                <input type="text" id="img_url" name="img_url" value={destination.img_url} onChange={handleChange}/><br />
-                <br/>
-
-                <label>Flag Image URL</label><br />
-                <input type="text" id="flag_img_url" name="flag_img_url" value={destination.flag_img_url} onChange={handleChange}/><br />
-                <br/>
-
-                <label>Trip ID</label><br />
-                <input type="text" id="flag_img_url" name="flag_img_url" value={trip_id} readOnly/><br />
-                <br/>
-
-                <input type="submit" value="Submit" onClick={createDestination} />
-            </form>
-        </div>
-    )
-}
-
-export default CreateDestination
+export default CreateDestination;
